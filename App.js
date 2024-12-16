9f02dd7f0ea2e0f913a23bf9e75be5b979010eea
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2024-12-16 19:46:12 +0500

    Age Calculator

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-function calculateAge(){
-    let biethday = document.getElementById("date").value 
+function calculateAge() {
+    let biethday = document.getElementById("date").value
     let birth = new Date(biethday);
-    
+
 
     let newdate = new Date();
     // console.log(newdate);
@@ -9,15 +9,17 @@ function calculateAge(){
     let month = newdate.getMonth() - birth.getMonth();
     let year = newdate.getFullYear() - birth.getFullYear();
 
-    let totalmins = Math.floor((newdate - birth) / (1000*60))
+    let totalmins = Math.floor((newdate - birth) / (1000 * 60))
     let totaldays = Math.floor((newdate - birth) / (1000 * 60 * 60 * 24));
+    let totalsec = Math.floor((newdate - birth) / (1000));
 
     document.getElementById("result").innerHTML = `
     <h1> You have been on Earth for:</h1>
     <h3> ${year} Years </h3>
     <h3> ${month} Months </h3>
     <h3> ${totaldays} Days </h3> 
-    <h3> ${totalmins} Minutes  `
+    <h3> ${totalmins} Minutes
+    <h3> ${totalsec} Seconds  `
 
 
 }
